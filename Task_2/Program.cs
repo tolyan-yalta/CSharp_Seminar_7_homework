43d55100069c1d1824d4440ffd6538b5dbd4670b
anatoly <tolyan.yalta@yandex.ru>
2022-09-10 05:16:07 +0300

    Add Task_2. The program that finds the element in the matrix

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -21,7 +21,7 @@ int[,] matrix = new int[rows, columns];
 
 for(int i = 0; i < matrix.GetLength(0); i++)
 {
-    for(int j = 0; j < matrix.GetLength(0); j++)
+    for(int j = 0; j < matrix.GetLength(1); j++)
     {
         matrix[i, j] = random_number(min, max);
         Console.Write($"\t{matrix[i, j]} ");
